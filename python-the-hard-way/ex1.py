9aff7f167c3e64bcd8f57ad4f929033786f8c64a
Mohamed T. Ismail <mohamed.t.ismail@gmail.com>
2014-09-14 02:41:57 -0400

    Creating repo for python practice exercises from different books.
    Starting Learn Python The Hard Way Exercises

---
+++ python-the-hard-way/ex1.py
@@ -0,0 +1,15 @@
+
+
+# print "Hello World"
+# print "Hello Again."
+print "THis is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Another line..."
+
+
+
+
+
+
